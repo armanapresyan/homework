6c4f624d63917b0a90451a5f941afd774beec768
armanapresyan <99826177+armanapresyan@users.noreply.github.com>
2022-03-20 13:53:25 +0400

    Add files via upload

--- homework4.js
+++ homework4.js
@@ -1,5 +1,5 @@
 //1.  Given three numbers a, b (a ≤ b) and step. Create an array of evenly spaced elements starting from a to b spaced by step.
-/* function stepArr(a, b, step) {
+function stepArr(a, b, step) {
   let arr = [];
 
   for (let i = a; i <= b; i += step) {
@@ -9,9 +9,10 @@
   console.log(arr);
 }
 
-stepArr(1, 5, 0.5); */
+stepArr(1, 5, 0.5); 
 //2. Given a string and a symbol. Find the number of occurrences of the symbol in the string.
-/* function findNumberSymbol(string, a) {
+
+function findNumberSymbol(string, a) {
   let count = 0;
 
   for (let i = 0; i < string.length; i++) {
@@ -23,9 +24,11 @@ stepArr(1, 5, 0.5); */
   return count;
 }
 
-console.log(findNumberSymbol("dsscfd cdsftatttttttstt", "t")); */
-//3. Given a string. Check whether the string is palindrome or not.  ???
-/* function checkPalidrom(string) {
+console.log(findNumberSymbol("dsscfd cdsftatttttttstt", "t"));
+
+//3. Given a string. Check whether the string is palindrome or not. 
+
+ function checkPalidrom(string) {
   let len = string.length;
 
   for (let i = 0; i <= string.length / 2; i++) {
@@ -37,9 +40,10 @@ console.log(findNumberSymbol("dsscfd cdsftatttttttstt", "t")); */
   return "Yes";
 }
 
-console.log(checkPalidrom("Asddsa")); */
+console.log(checkPalidrom("Asddsa")); 
+
 //4. Given an array of numbers. Find the maximum element in array.
-/* function maxElementArr(arr) {
+function maxElementArr(arr) {
   let max = arr[0];
 
   for (let i = 1; i < arr.length; i++) {
@@ -51,9 +55,11 @@ console.log(checkPalidrom("Asddsa")); */
   return max;
 }
 
-console.log(maxElementArr([2, 3, 4, 5, 6, 7, 45])); */
+console.log(maxElementArr([2, 3, 4, 5, 6, 7, 45])); 
+
 //5. Given an array. Create the array which elements are products between two neighbours.
-/* function neighbours(arr) {
+
+function neighbours(arr) {
   let arr1 = [];
   for (let i = 0; i < arr.length - 1; i++) {
     arr1.push(arr[i] * arr[i + 1]);
@@ -61,9 +67,11 @@ console.log(maxElementArr([2, 3, 4, 5, 6, 7, 45])); */
   return arr1;
 }
 
-console.log(neighbours([3, 7, 12, 5, 20, 0])) */
-//6. Given a string and symbols. Change first symbol by the second one in the string.??
-/* function replaceSymbol(str, a, b) {
+console.log(neighbours([3, 7, 12, 5, 20, 0]));
+
+//6. Given a string and symbols. Change first symbol by the second one in the string.
+
+function replaceSymbol(str, a, b) {
   while (str.indexOf(a) !== -1) {
     str = str.replace(a, b);
   }
@@ -71,9 +79,10 @@ console.log(neighbours([3, 7, 12, 5, 20, 0])) */
   return str;
 }
 
-console.log(replaceSymbol("abd sdlkjf lsdlfasdmlka", "a", "_")); */
+console.log(replaceSymbol("abd sdlkjf lsdlfasdmlka", "a", "_")); 
+
 //7. Print the following number pattern:
-/* 
+
 let a = +prompt("say number");
 let result = "";
 let row = "";
@@ -94,4 +103,4 @@ for (let i = a; i >= 1; i--) {
 
 console.log(result);
 
- */
+
